f4fb031693d43e0d65e0eab0ead2e3c22fd1584e
abel-otegbola <abel15655@gmail.com>
2025-09-19 11:48:25 +0100

    fix: SVG error to use type

--- src/assets/icons/bell.tsx
+++ src/assets/icons/bell.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from "react";
+import { type SVGProps } from "react";
 
 const BellIcon = (props: SVGProps<SVGSVGElement>) => (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
